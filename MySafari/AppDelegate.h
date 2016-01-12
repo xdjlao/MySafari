1696abd073ff6aecf1c97a19193e7d1026ed3117
xdjlao <jerry@flatie.com>
2016-01-12 13:51:07 -0800

    initial commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Jerry on 1/12/16.
+//  Copyright © 2016 Jerry Lao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySafari/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by Jerry on 1/12/16.
+//  Copyright © 2016 Jerry Lao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
